d63325323606e32a05d031d32a1245c2a895e558
Yuriy <yurchest@email.com>
2022-05-18 03:37:37 +0300

    commit

--- func.cpp
+++ func.cpp
@@ -4,8 +4,8 @@ extern "C"{
 float func_cpp(float *x, int len){
     float sum = 0;
     for (int i = 0; i < len; i++){
-		sum = sum +  x[i];
+		sum +=  x[i];
 	}
-    return (sum/len);
+    return (sum / len);
     }
 }
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import math
 from ctypes import *
 
 import numpy as np
@@ -31,24 +31,29 @@ if __name__ == "__main__":
 
     ##  Numpy
     print('\n')
-    array_np = np.array(array)
+
     t0 = time.time()
+    array_np = np.array(array)
     result_numpy = np.mean(array_np)
     t1 = time.time()
     print(f"Numpy Func\nTime = {(t1 - t0) * 1000} ms \nResult = {result_numpy}")
 
     ## C++ dll import
 
-    print('\n')
+    print("\n")
+    os.system("g++ -fPIC -shared -o lib.so func.cpp")
     path = os.path.join(os.getcwd(), 'lib.so')
-    cpp = CDLL(path, winmode=1)
-    # cpp.func_cpp.argtypes = [POINTER(c_float * len(array)), c_ulonglong]
+    cpp = CDLL(path)
+    cpp.func_cpp.restype = c_float
+    cpp.func_cpp.argtypes = [POINTER(c_float * len(array)), c_ulonglong]
 
     array_x = (c_float * len(array))(*array)
+    leng = c_ulonglong(len(array))
 
-    # print(array_x, hex(id(array_x)), byref(array_x), pointer(array_x))
+    print(f"{hex(id(array_x))} --- {byref(array_x)} --- {byref(array_x)}")
 
     t0 = time.time()
-    result_cpp_import = cpp.func_cpp(array_x, len(array))
+    result_cpp_import = cpp.func_cpp(byref(array_x), leng)
     t1 = time.time()
     print(f"CPP import Func\nTime = {(t1 - t0) * 1000} ms \nResult = {result_cpp_import}")
+
